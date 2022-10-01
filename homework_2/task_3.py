9e784a8b54314f59d0d42566e4996c6954f072e1
Darya Nesterova <darya999r@mail.ru>
2022-10-01 15:58:37 +0300

    homework2 task 3 4

--- homework_2/task_3.py
+++ homework_2/task_3.py
@@ -3,3 +3,29 @@
 # выводится на экран, затем перемешивается, опять выводится на экран. 
 # SHUFFLE нельзя юзать!
 
+import random
+
+def fillArr(arr,l):
+    arr = []
+    for i in range(0, l):
+        arr.append(random.randint(0,30))
+    return arr
+
+def shufNumbers(array):
+    temp = 0
+    for i in range(len(array)):
+        if i%2==0:
+            temp = array[i]
+            array[i] = array[i+1]
+            array[i+1] = temp
+    return array
+
+list1 = []
+length = 10      
+list1 = fillArr(list1, length)
+
+print(list1)
+print()
+print(shufNumbers(list1))
+
+
--- homework_2/task_4.py
+++ homework_2/task_4.py
@@ -3,3 +3,39 @@
 # и координаты двух точек и находит 
 # расстояние между ними в N-мерном пространстве.
 
+def enterX(n):
+    listX = []
+    for i in range(1, n+1):
+        listX.append(float(input("Enter the coordinates of the first point: ")))
+    return listX
+
+def enterY(n):
+    listY = []
+    for i in range(1, n+1):
+        listY.append(float(input("Enter the coordinates of the second point: ")))
+    return listY
+
+def distansEuclid(n, arr1, arr2):
+    res = 0
+    sum = 0
+    counter = 0
+    while counter<=(n-1):
+        res = (arr2[counter]-arr1[counter])**2
+        sum += res
+        counter += 1
+    res1 = sum**0.5
+    return res1
+
+try:
+    N = int(input("Enter number N: "))
+
+    list1 = enterX(N)
+    list2 = enterY(N)
+    print("Coordinates of the first point: ", list1)
+    print("Coordinates of the second point: ", list2)
+
+    result = distansEuclid(N,list1,list2)
+    print("Distance between two points: ", round(result,3))
+except:
+    print("For N: enter integer number!"
+    +"\nFor coordinates: enter integer or float numbers!")
