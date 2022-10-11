5d0038472e3e4a2b6e3dc3f0f92371e6aebf22f7
Darya Nesterova <darya999r@mail.ru>
2022-10-11 19:59:55 +0300

    homework5 task 3

--- homework_5/task_3.py
+++ homework_5/task_3.py
@@ -1,1 +1,12 @@
 # задача 3. Напишите программу, удаляющую из текста все слова, содержащие "абв".
+
+list_1 = 'Читабвйте самые интересные и обсуждабвемые посты по теме Текст. Личный опыт, познавабвтельные статьи, забвавные фото и видео. Или поделитесь своей историей.'
+print("Specified text:\n", list_1)
+list_2 = list(list_1.split(' '))
+searching = 'абв'
+for elem in list_2:
+    if searching in elem:
+        list_2.remove(elem)
+
+print("Corrected text:")
+print(" ".join(map(str,list_2)))
