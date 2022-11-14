92b7d58e7f6df331af7107d97502cf23b1d4bfe1
Darya Nesterova <darya999r@mail.ru>
2022-11-14 14:18:38 +0300

    homework 10 readme

--- homework_10/main.py
+++ homework_10/main.py
@@ -2,7 +2,7 @@ import telebot
 from telebot import types
 import json
 
-API_TOKEN='.......'
+API_TOKEN='5584990622:AAEX2sJj3SzL6rO0fa_mfqNi1M8XD1l68v8'
 bot = telebot.TeleBot(API_TOKEN)
 
 print('bot is start')
